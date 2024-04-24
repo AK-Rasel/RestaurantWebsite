181a14e8469deadf8a2181ca6bfbd5de85de5971
MD.Abdul Khalek Rasel <raselak101010@gmail.com>
2024-04-24 18:46:58 +0600

    navbar resposive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
   return (
     <header className="max-w-screen-2xl container mx-auto">
       <div className="navbar xl:px-24 ">
-        <div className="navbar-start">
+        <div className="lg:navbar-start w-3/4  justify-between">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
@@ -75,8 +75,8 @@ const Navbar = () => {
               {NavItems}
             </ul>
           </div>
-          <a href="/" className="flex  items-center">
-            <img className="h-10" src={logo} alt="" />
+          <a href="/">
+            <img className="h-8 lg:h-10" src={logo} alt="" />
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -86,61 +86,63 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          {/* search */}
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          {/* Cart */}
-          <div className="dropdown dropdown-end mr-3">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle"
-            >
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-sm py-[10px] bg-orenge indicator-item">
-                  8
-                </span>
+          <div className="hidden lg:flex">
+            {/* search */}
+            <button className=" btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+            {/* Cart */}
+            <div className="dropdown dropdown-end mr-3">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+              >
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm py-[10px] bg-orenge indicator-item">
+                    8
+                  </span>
+                </div>
               </div>
-            </div>
-            <div
-              tabIndex={0}
-              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow border-orenge"
-            >
-              <div className="card-body">
-                <span className="font-bold text-lg">8 Items</span>
-                <span className="text-red font-semibold">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn rounded-full px-4 hover:bg-transparent hover:border-orenge  bg-orenge btn-block">
-                    View cart
-                  </button>
+              <div
+                tabIndex={0}
+                className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow border-orenge"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">8 Items</span>
+                  <span className="text-red font-semibold">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button className="btn rounded-full px-4 hover:bg-transparent hover:border-orenge  bg-orenge btn-block">
+                      View cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
